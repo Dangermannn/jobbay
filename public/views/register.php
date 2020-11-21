e7fd301790f09072d6b1dda3500acd8f8f71d7b0
Dangermannn <dangermanplyt@gmail.com>
2020-11-21 13:04:20 +0100

    Fixed appearing horizontal scrollbar

--- public/views/register.php
+++ public/views/register.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/register.css">
     <title>Login page</title>
 </head>
 <body>
